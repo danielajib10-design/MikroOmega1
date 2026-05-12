6db52fdb25f429d97597d0728ca20aa04762cc4a
Daniel <danielajib10@gmail.com>
2026-05-12 09:00:36 +0200

    upraveni druheho okna tak aby kdyz ho zavru aby se zavrelo pouze to okno a ne vsechna okna

--- src/Quiz/QuizFrame.java
+++ src/Quiz/QuizFrame.java
@@ -7,10 +7,11 @@ public class QuizFrame {
     private JFrame jFrame;
 
     public QuizFrame() {
+
         jFrame = new JFrame("Quiz");
         jFrame.setSize(500, 500);
         jFrame.setLocationRelativeTo(null);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         jFrame.add(new QuizPanel());
         jFrame.setVisible(true);
     }
